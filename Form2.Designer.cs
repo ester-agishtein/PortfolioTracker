f3ab142387ae68e51a571d4a873917b42602633a
HelenSutton <38437834+HelenSutton@users.noreply.github.com>
2020-12-25 15:42:19 -0500

    Added a form to add trades

---
+++ Form2.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace FinanceProject
+{
+    partial class TradeFom
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addButton = new System.Windows.Forms.Button();
+            this.tickerLabel = new System.Windows.Forms.Label();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
+            this.quantityTextBox = new System.Windows.Forms.TextBox();
+            this.priceLabel = new System.Windows.Forms.Label();
+            this.quantityLabel = new System.Windows.Forms.Label();
+            this.tickerComboBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(637, 359);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 23);
+            this.addButton.TabIndex = 0;
+            this.addButton.Text = "Add Trade";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tickerLabel
+            // 
+            this.tickerLabel.AutoSize = true;
+            this.tickerLabel.Location = new System.Drawing.Point(66, 76);
+            this.tickerLabel.Name = "tickerLabel";
+            this.tickerLabel.Size = new System.Drawing.Size(51, 20);
+            this.tickerLabel.TabIndex = 1;
+            this.tickerLabel.Text = "Ticker";
+            // 
+            // priceTextBox
+            // 
+            this.priceTextBox.Location = new System.Drawing.Point(135, 143);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(100, 26);
+            this.priceTextBox.TabIndex = 3;
+            // 
+            // quantityTextBox
+            // 
+            this.quantityTextBox.Location = new System.Drawing.Point(135, 212);
+            this.quantityTextBox.Name = "quantityTextBox";
+            this.quantityTextBox.Size = new System.Drawing.Size(100, 26);
+            this.quantityTextBox.TabIndex = 4;
+            // 
+            // priceLabel
+            // 
+            this.priceLabel.AutoSize = true;
+            this.priceLabel.Location = new System.Drawing.Point(66, 149);
+            this.priceLabel.Name = "priceLabel";
+            this.priceLabel.Size = new System.Drawing.Size(44, 20);
+            this.priceLabel.TabIndex = 5;
+            this.priceLabel.Text = "Price";
+            this.priceLabel.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // quantityLabel
+            // 
+            this.quantityLabel.AutoSize = true;
+            this.quantityLabel.Location = new System.Drawing.Point(66, 212);
+            this.quantityLabel.Name = "quantityLabel";
+            this.quantityLabel.Size = new System.Drawing.Size(68, 20);
+            this.quantityLabel.TabIndex = 6;
+            this.quantityLabel.Text = "Quantity";
+            // 
+            // tickerComboBox
+            // 
+            this.tickerComboBox.FormattingEnabled = true;
+            this.tickerComboBox.Location = new System.Drawing.Point(135, 68);
+            this.tickerComboBox.Name = "tickerComboBox";
+            this.tickerComboBox.Size = new System.Drawing.Size(121, 28);
+            this.tickerComboBox.TabIndex = 7;
+            // 
+            // TradeFom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tickerComboBox);
+            this.Controls.Add(this.quantityLabel);
+            this.Controls.Add(this.priceLabel);
+            this.Controls.Add(this.quantityTextBox);
+            this.Controls.Add(this.priceTextBox);
+            this.Controls.Add(this.tickerLabel);
+            this.Controls.Add(this.addButton);
+            this.Name = "TradeFom";
+            this.Text = "Add Trade";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Label tickerLabel;
+        private System.Windows.Forms.TextBox priceTextBox;
+        private System.Windows.Forms.TextBox quantityTextBox;
+        private System.Windows.Forms.Label priceLabel;
+        private System.Windows.Forms.Label quantityLabel;
+        private System.Windows.Forms.ComboBox tickerComboBox;
+    }
+}
