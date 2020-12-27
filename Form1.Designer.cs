1b08f1fe1b15d0d5453062831bbaf508181c09b5
shiraalter <shiraalter@gmail.com>
2020-12-26 22:02:21 -0500

    changed UI format

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,8 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.DailyValuesTableButtonPanel = new System.Windows.Forms.Panel();
             this.endDateLabel = new System.Windows.Forms.Label();
             this.endDateTimePicker = new System.Windows.Forms.DateTimePicker();
@@ -52,7 +52,7 @@
             // 
             // DailyValuesTableButtonPanel
             // 
-            this.DailyValuesTableButtonPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.DailyValuesTableButtonPanel.BackColor = System.Drawing.Color.Tan;
             this.DailyValuesTableButtonPanel.Controls.Add(this.endDateLabel);
             this.DailyValuesTableButtonPanel.Controls.Add(this.endDateTimePicker);
             this.DailyValuesTableButtonPanel.Controls.Add(this.TransactionsButton);
@@ -60,9 +60,10 @@
             this.DailyValuesTableButtonPanel.Controls.Add(this.TickerLabel);
             this.DailyValuesTableButtonPanel.Controls.Add(this.TransactionsStartDatePicker);
             this.DailyValuesTableButtonPanel.Controls.Add(this.comboBox1);
-            this.DailyValuesTableButtonPanel.Location = new System.Drawing.Point(7, 12);
+            this.DailyValuesTableButtonPanel.Location = new System.Drawing.Point(428, 11);
+            this.DailyValuesTableButtonPanel.Margin = new System.Windows.Forms.Padding(2);
             this.DailyValuesTableButtonPanel.Name = "DailyValuesTableButtonPanel";
-            this.DailyValuesTableButtonPanel.Size = new System.Drawing.Size(1852, 107);
+            this.DailyValuesTableButtonPanel.Size = new System.Drawing.Size(1018, 78);
             this.DailyValuesTableButtonPanel.TabIndex = 4;
             // 
             // endDateLabel
@@ -70,9 +71,10 @@
             this.endDateLabel.AutoSize = true;
             this.endDateLabel.BackColor = System.Drawing.SystemColors.Control;
             this.endDateLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.endDateLabel.Location = new System.Drawing.Point(1087, 12);
+            this.endDateLabel.Location = new System.Drawing.Point(574, 20);
+            this.endDateLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.endDateLabel.Name = "endDateLabel";
-            this.endDateLabel.Size = new System.Drawing.Size(225, 55);
+            this.endDateLabel.Size = new System.Drawing.Size(112, 29);
             this.endDateLabel.TabIndex = 17;
             this.endDateLabel.Text = "End Date";
             // 
@@ -80,17 +82,20 @@
             // 
             this.endDateTimePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.endDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.endDateTimePicker.Location = new System.Drawing.Point(1347, 10);
+            this.endDateTimePicker.Location = new System.Drawing.Point(690, 18);
+            this.endDateTimePicker.Margin = new System.Windows.Forms.Padding(2);
             this.endDateTimePicker.Name = "endDateTimePicker";
-            this.endDateTimePicker.Size = new System.Drawing.Size(290, 61);
+            this.endDateTimePicker.Size = new System.Drawing.Size(147, 34);
             this.endDateTimePicker.TabIndex = 16;
             this.endDateTimePicker.Value = new System.DateTime(2020, 12, 24, 0, 0, 0, 0);
             // 
             // TransactionsButton
             // 
-            this.TransactionsButton.Location = new System.Drawing.Point(1678, 9);
+            this.TransactionsButton.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TransactionsButton.Location = new System.Drawing.Point(892, 10);
+            this.TransactionsButton.Margin = new System.Windows.Forms.Padding(2);
             this.TransactionsButton.Name = "TransactionsButton";
-            this.TransactionsButton.Size = new System.Drawing.Size(156, 68);
+            this.TransactionsButton.Size = new System.Drawing.Size(111, 55);
             this.TransactionsButton.TabIndex = 13;
             this.TransactionsButton.Text = "Get";
             this.TransactionsButton.UseVisualStyleBackColor = true;
@@ -100,9 +105,10 @@
             this.StartDate.AutoSize = true;
             this.StartDate.BackColor = System.Drawing.SystemColors.Control;
             this.StartDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.StartDate.Location = new System.Drawing.Point(485, 13);
+            this.StartDate.Location = new System.Drawing.Point(275, 20);
+            this.StartDate.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.StartDate.Name = "StartDate";
-            this.StartDate.Size = new System.Drawing.Size(240, 55);
+            this.StartDate.Size = new System.Drawing.Size(118, 29);
             this.StartDate.TabIndex = 12;
             this.StartDate.Text = "Start Date";
             // 
@@ -111,9 +117,10 @@
             this.TickerLabel.AutoSize = true;
             this.TickerLabel.BackColor = System.Drawing.SystemColors.Control;
             this.TickerLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TickerLabel.Location = new System.Drawing.Point(1, 14);
+            this.TickerLabel.Location = new System.Drawing.Point(18, 18);
+            this.TickerLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TickerLabel.Name = "TickerLabel";
-            this.TickerLabel.Size = new System.Drawing.Size(154, 55);
+            this.TickerLabel.Size = new System.Drawing.Size(81, 29);
             this.TickerLabel.TabIndex = 11;
             this.TickerLabel.Text = "Ticker";
             // 
@@ -122,91 +129,106 @@
             this.TransactionsStartDatePicker.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 8.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TransactionsStartDatePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.TransactionsStartDatePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.TransactionsStartDatePicker.Location = new System.Drawing.Point(760, 9);
+            this.TransactionsStartDatePicker.Location = new System.Drawing.Point(397, 18);
+            this.TransactionsStartDatePicker.Margin = new System.Windows.Forms.Padding(2);
             this.TransactionsStartDatePicker.Name = "TransactionsStartDatePicker";
-            this.TransactionsStartDatePicker.Size = new System.Drawing.Size(297, 61);
+            this.TransactionsStartDatePicker.Size = new System.Drawing.Size(150, 34);
             this.TransactionsStartDatePicker.TabIndex = 10;
             // 
             // comboBox1
             // 
             this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(196, 9);
+            this.comboBox1.Location = new System.Drawing.Point(103, 15);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(239, 62);
+            this.comboBox1.Size = new System.Drawing.Size(122, 37);
             this.comboBox1.TabIndex = 9;
             // 
             // TransactionsLabel
             // 
             this.TransactionsLabel.AutoSize = true;
-            this.TransactionsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TransactionsLabel.Location = new System.Drawing.Point(186, 198);
+            this.TransactionsLabel.BackColor = System.Drawing.Color.SeaShell;
+            this.TransactionsLabel.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TransactionsLabel.Location = new System.Drawing.Point(101, 102);
+            this.TransactionsLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TransactionsLabel.Name = "TransactionsLabel";
-            this.TransactionsLabel.Size = new System.Drawing.Size(457, 55);
+            this.TransactionsLabel.Size = new System.Drawing.Size(298, 37);
             this.TransactionsLabel.TabIndex = 8;
             this.TransactionsLabel.Text = "Transactions Report";
             // 
             // TransactionsReportDataGridView
             // 
             this.TransactionsReportDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.TransactionsReportDataGridView.Location = new System.Drawing.Point(6, 291);
+            this.TransactionsReportDataGridView.Location = new System.Drawing.Point(25, 151);
+            this.TransactionsReportDataGridView.Margin = new System.Windows.Forms.Padding(2);
             this.TransactionsReportDataGridView.Name = "TransactionsReportDataGridView";
             this.TransactionsReportDataGridView.RowHeadersWidth = 102;
             this.TransactionsReportDataGridView.RowTemplate.Height = 40;
-            this.TransactionsReportDataGridView.Size = new System.Drawing.Size(826, 1312);
+            this.TransactionsReportDataGridView.Size = new System.Drawing.Size(469, 677);
             this.TransactionsReportDataGridView.TabIndex = 5;
             // 
             // PnLReportDataGridView
             // 
             this.PnLReportDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.PnLReportDataGridView.Location = new System.Drawing.Point(865, 291);
+            this.PnLReportDataGridView.Location = new System.Drawing.Point(553, 150);
+            this.PnLReportDataGridView.Margin = new System.Windows.Forms.Padding(2);
             this.PnLReportDataGridView.Name = "PnLReportDataGridView";
             this.PnLReportDataGridView.RowHeadersWidth = 102;
             this.PnLReportDataGridView.RowTemplate.Height = 40;
-            this.PnLReportDataGridView.Size = new System.Drawing.Size(934, 1312);
+            this.PnLReportDataGridView.Size = new System.Drawing.Size(467, 677);
             this.PnLReportDataGridView.TabIndex = 6;
             // 
             // chart1
             // 
-            chartArea2.Name = "ChartArea1";
-            this.chart1.ChartAreas.Add(chartArea2);
-            this.chart1.Location = new System.Drawing.Point(1815, 291);
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            this.chart1.Location = new System.Drawing.Point(1112, 151);
+            this.chart1.Margin = new System.Windows.Forms.Padding(2);
             this.chart1.Name = "chart1";
-            series2.ChartArea = "ChartArea1";
-            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
-            series2.Name = "Series1";
-            series2.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
-            this.chart1.Series.Add(series2);
-            this.chart1.Size = new System.Drawing.Size(951, 1312);
+            series3.ChartArea = "ChartArea1";
+            series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series3.Name = "Series1";
+            series3.XValueType = System.Windows.Forms.DataVisualization.Charting.ChartValueType.Date;
+            this.chart1.Series.Add(series3);
+            this.chart1.Size = new System.Drawing.Size(641, 677);
             this.chart1.TabIndex = 7;
             this.chart1.Text = "chartPrices";
             // 
             // InsertNewTradesButton
             // 
-            this.InsertNewTradesButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.900001F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InsertNewTradesButton.Location = new System.Drawing.Point(1865, 12);
+            this.InsertNewTradesButton.BackColor = System.Drawing.Color.Tan;
+            this.InsertNewTradesButton.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.InsertNewTradesButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.InsertNewTradesButton.Location = new System.Drawing.Point(624, 865);
+            this.InsertNewTradesButton.Margin = new System.Windows.Forms.Padding(2);
             this.InsertNewTradesButton.Name = "InsertNewTradesButton";
-            this.InsertNewTradesButton.Size = new System.Drawing.Size(901, 273);
+            this.InsertNewTradesButton.Size = new System.Drawing.Size(541, 108);
             this.InsertNewTradesButton.TabIndex = 8;
             this.InsertNewTradesButton.Text = "Insert New Trade";
-            this.InsertNewTradesButton.UseVisualStyleBackColor = true;
+            this.InsertNewTradesButton.UseVisualStyleBackColor = false;
             this.InsertNewTradesButton.Click += new System.EventHandler(this.InsertNewTradesButton_Click);
             // 
             // PnLlabel
             // 
             this.PnLlabel.AutoSize = true;
-            this.PnLlabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.1F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.PnLlabel.Location = new System.Drawing.Point(1181, 198);
+            this.PnLlabel.BackColor = System.Drawing.Color.SeaShell;
+            this.PnLlabel.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PnLlabel.Location = new System.Drawing.Point(723, 102);
+            this.PnLlabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.PnLlabel.Name = "PnLlabel";
-            this.PnLlabel.Size = new System.Drawing.Size(268, 55);
+            this.PnLlabel.Size = new System.Drawing.Size(167, 37);
             this.PnLlabel.TabIndex = 9;
             this.PnLlabel.Text = "PnL Report";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2795, 1644);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = global::FinanceProject.Properties.Resources.vs_money;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1804, 996);
             this.Controls.Add(this.PnLlabel);
             this.Controls.Add(this.InsertNewTradesButton);
             this.Controls.Add(this.chart1);
@@ -214,6 +236,7 @@
             this.Controls.Add(this.TransactionsReportDataGridView);
             this.Controls.Add(this.DailyValuesTableButtonPanel);
             this.Controls.Add(this.TransactionsLabel);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.DailyValuesTableButtonPanel.ResumeLayout(false);
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Drawing.Drawing2D;
 
 namespace FinanceProject
 {
@@ -23,5 +24,6 @@ namespace FinanceProject
             tradeForm.Show();
 
         }
+
     }
 }
