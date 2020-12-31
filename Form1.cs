8ca0dc475234c3bf035e4f6d5cb76d3fddad8541
Ester Agishtein <eagishtein@gmail.com>
2020-12-30 19:54:34 -0800

    Drop down is now populated

--- Form1.cs
+++ Form1.cs
@@ -8,9 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Drawing.Drawing2D;
-using System.Data.SqlClient;
 using System.Configuration;
-
+using System.Data.SqlClient;
 namespace FinanceProject
 {
     public partial class Form1 : Form
@@ -18,6 +17,42 @@ namespace FinanceProject
         public Form1()
         {
             InitializeComponent();
+            Console.WriteLine("hi");
+            //TODO make a check that a trade is inserted before closing and adding it to the trades table/PnL chart
+            SqlConnection sqlCon = null;
+            //get database parameters from App.config file
+            String strServer = ConfigurationManager.AppSettings["server"];
+            String strDatabase = ConfigurationManager.AppSettings["database"];
+            String strLogin = ConfigurationManager.AppSettings["login"];
+            String strPassword = ConfigurationManager.AppSettings["password"];
+
+            String strConnect = $"Server={strServer};Database={strDatabase};User Id={strLogin};Password={strPassword};";
+            sqlCon = new SqlConnection(strConnect);
+            sqlCon.Open();
+            //get values
+
+
+            //set up call to stored procedure
+            SqlCommand sqlCmd = new SqlCommand("get_available_tickers", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.ExecuteNonQuery();
+
+            SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
+            DataSet dataset = new DataSet();
+            da.Fill(dataset, "table1");
+         
+            foreach (DataTable table in dataset.Tables)
+            {
+                foreach (DataRow row in table.Rows)
+                {
+                    foreach (object item in row.ItemArray)
+                    {
+                   
+                        comboBox1.Items.Add(item.ToString());
+                    }
+                }
+            }
+
         }
 
         private void InsertNewTradesButton_Click(object sender, EventArgs e)
@@ -27,111 +62,21 @@ namespace FinanceProject
 
         }
 
-        private void TransactionsButton_Click(object sender, EventArgs e)
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            SqlConnection sqlCon = null;
-            //get database parameters from App.config file
-            String strServer = ConfigurationManager.AppSettings["server"];
-            String strDatabase = ConfigurationManager.AppSettings["database"];
-            String strLogin = ConfigurationManager.AppSettings["login"];
-            String strPassword = ConfigurationManager.AppSettings["password"];
-            //open connection to database
-            String strConnect = $"Server={strServer};Database={strDatabase};User Id={strLogin};Password={strPassword};";
-            sqlCon = new SqlConnection(strConnect);
-            sqlCon.Open();
-            //get values
-            string ticker = comboBox1.ValueMember;
-            DateTime startDate = TransactionsStartDatePicker.Value;
-            DateTime endDate = endDateTimePicker.Value;
-            generatePnL(sqlCon, ticker, startDate, endDate);
-            generatePortfolio(sqlCon, startDate, endDate);
-            generateTransaction(sqlCon);
-            getDailyYield(sqlCon, ticker, startDate, endDate);
+            //Actually do something/get selected value
+            string value = comboBox1.SelectedItem.ToString();
+            Console.WriteLine(value);
         }
 
-
-        private void generatePnL(SqlConnection sqlCon, String ticker, DateTime startDate, DateTime endDate)
+        private void endDateTimePicker_ValueChanged(object sender, EventArgs e)
         {
-            //set up call to stored procedure
-            SqlCommand sqlCmd = new SqlCommand("ticker_daily_pl_report", sqlCon);
-            sqlCmd.CommandType = CommandType.StoredProcedure;
-            sqlCmd.Parameters.Add("@Ticker", System.Data.SqlDbType.VarChar).Value = ticker;
-            sqlCmd.Parameters.Add("@Start Date", System.Data.SqlDbType.DateTime).Value = startDate;
-            sqlCmd.Parameters.Add("@End Date", System.Data.SqlDbType.DateTime).Value = endDate;
-            //execute stored procedure
-            sqlCmd.ExecuteNonQuery();
-            //get data returned by stored procedure and display it
 
-            //----not sure what to do here-------
-            SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
-            DataSet dataset = new DataSet();
-            da.Fill(dataset, "table1");
-            //PnLReportDataGridView.AutoGenerateColumns = true;
-            //PnLReportDataGridView.DataSource = dataset.Tables["table1"];
         }
-
-        private void generatePortfolio(SqlConnection sqlCon, DateTime startDate, DateTime endDate)
-        {
-            //set up call to stored procedure
-            SqlCommand sqlCmd = new SqlCommand("portfolio_daily_pl_report", sqlCon);
-            sqlCmd.CommandType = CommandType.StoredProcedure;
-            sqlCmd.Parameters.Add("@Start Date", System.Data.SqlDbType.DateTime).Value = startDate;
-            sqlCmd.Parameters.Add("@End Date", System.Data.SqlDbType.DateTime).Value = endDate;
-            //execute stored procedure
-            sqlCmd.ExecuteNonQuery();
-            //get data returned by stored procedure and display it
-
-            //----not sure what to do here-------
-            SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
-            DataSet dataset = new DataSet();
-            da.Fill(dataset, "table1");
-            //____________.AutoGenerateColumns = true;
-            //____________.DataSource = dataset.Tables["table1"];
-        }
-
-        private void generateTransaction(SqlConnection sqlCon)
-        {
-            //set up call to stored procedure
-            SqlCommand sqlCmd = new SqlCommand("transaction_report", sqlCon);
-            sqlCmd.CommandType = CommandType.StoredProcedure;
-            //execute stored procedure
-            sqlCmd.ExecuteNonQuery();
-            //get data returned by stored procedure and display it
-
-            //----not sure what to do here-------
-            SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
-            DataSet dataset = new DataSet();
-            da.Fill(dataset, "table1");
-            //____________.AutoGenerateColumns = true;
-            //____________.DataSource = dataset.Tables["table1"];
-        }
-
-        private void getDailyYield(SqlConnection sqlCon, String ticker, DateTime startDate, DateTime endDate)
-        {
-            //set up call to stored procedure
-            SqlCommand sqlCmd = new SqlCommand("get_ticker_daily_yield", sqlCon);
-            sqlCmd.CommandType = CommandType.StoredProcedure;
-            sqlCmd.Parameters.Add("@Ticker", System.Data.SqlDbType.VarChar).Value = ticker;
-            sqlCmd.Parameters.Add("@Start Date", System.Data.SqlDbType.DateTime).Value = startDate;
-            sqlCmd.Parameters.Add("@End Date", System.Data.SqlDbType.DateTime).Value = endDate;
-            //execute stored procedure
-            sqlCmd.ExecuteNonQuery();
-            //get data returned by stored procedure and display it
-
-            //----not sure what to do here-------
-            SqlDataAdapter da = new SqlDataAdapter(sqlCmd);
-            DataSet dataset = new DataSet();
-            da.Fill(dataset, "table1");
-            //____________.AutoGenerateColumns = true;
-            //____________.DataSource = dataset.Tables["table1"];
-        }
-
-
 
         private void TransactionsStartDatePicker_ValueChanged(object sender, EventArgs e)
         {
 
         }
-
     }
 }
